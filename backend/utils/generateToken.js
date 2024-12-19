f213a414b870ad5b8ee2ece7c678565f8bbf0123
abdullah-dot-developer <abdullahdeveloper966@gmail.com>
2024-12-19 10:35:31 +0500

    secure = true

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,6 +7,6 @@ export const generateTokenAndSetCookie = (id, res) => {
         maxAge: 21 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development"
+        secure: true
     })
 }
