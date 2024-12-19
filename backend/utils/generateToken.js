2775a27e26a9cec7e5a585fcbcc421d3b6250270
abdullah-dot-developer <abdullahdeveloper966@gmail.com>
2024-12-19 10:40:59 +0500

    Error Resolved

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,8 @@ export const generateTokenAndSetCookie = (id, res) => {
     res.cookie("jwt", token, {
         maxAge: 21 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: true
+        sameSite: "none", // strict in dev mode
+        secure: true, // false in dev mode
+        path: "/",
     })
 }
